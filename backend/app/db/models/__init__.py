4d1071d9e3a0f21d5160ca2cfc573d96284908f9
MarkRyanGarcia <markrgarcia@csu.fullerton.edu>
2026-01-10 01:36:41 -0800

    fix naming convention

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -2,11 +2,11 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from .User import User
-from .Team import Team
-from .TeamMember import TeamMember
-from .TeamPoint import TeamPoint
-from .TeamSubmitAttempt import TeamSubmitAttempt
+from .user import User
+from .team import Team
+from .team_member import TeamMember
+from .team_point import TeamPoint
+from .team_submit_attempt import TeamSubmitAttempt
 
 __all__ = [
     "User",

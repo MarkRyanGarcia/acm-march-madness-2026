271212ef2cb5f604c208e90e448a74e60b58d61d
MarkRyanGarcia <markrgarcia@csu.fullerton.edu>
2026-01-10 10:05:49 -0800

    remove schemas init

--- backend/app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import UserCreate, UserOut
-from .team import TeamCreate, TeamOut
-from .team_member import TeamMemberOut
-
-__all__ = [
-    'UserCreate', 'UserOut',
-    'TeamCreate', 'TeamOut',
-    'TeamMemberOut',
-]

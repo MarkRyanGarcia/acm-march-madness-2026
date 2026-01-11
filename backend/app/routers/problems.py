6ee2dc6ef966f4f92204c539e97108d980815daa
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-01-10 17:04:01 -0800

    add basic styles to problem markdown descriptions

--- backend/app/routers/problems.py
+++ backend/app/routers/problems.py
@@ -8,7 +8,6 @@ router = APIRouter()
 
 @router.get("/problems/{day}")
 def get_problem(day: int):
-    print("CALLED THIS ENDPOINT")
     input_file_path = os.path.join("problems", "00-math-homework", "README.md")
 
     return FileResponse(
--- frontend/src/routes/problems/$day.tsx
+++ frontend/src/routes/problems/$day.tsx
@@ -1,5 +1,6 @@
 import { MarkdownHooks } from "react-markdown";
 import { createFileRoute } from "@tanstack/react-router";
+import type { Components } from "react-markdown";
 import { useProblem } from "@/client/problem/getProblem";
 
 export const Route = createFileRoute("/problems/$day")({
@@ -14,9 +15,28 @@ function RouteComponent() {
     return <div>Loading...</div>;
   }
 
+  const components: Components = {
+    h1: ({ children }) => <h2 className="font-bold text-3xl">{children}</h2>,
+    h2: ({ children }) => <h3 className="font-bold text-2xl">{children}</h3>,
+    blockquote: ({ children }) => (
+      <blockquote className="bg-sky-600 p-2 border-l-4 border-white pl-4">
+        {children}
+      </blockquote>
+    ),
+    pre: ({ children }) => (
+      <pre className="p-1 rounded-sm w-max bg-sky-600">{children}</pre>
+    ),
+    code: ({ children }) => (
+      <code className="bg-sky-600 rounded-sm px-1">{children}</code>
+    ),
+    li: ({ children }) => <li>- {children}</li>,
+  };
+
   return (
-    <article className="bg-sky-600 max-w-6xl mx-auto text-slate-200 p-8 my-8 rounded-lg text-lg flex flex-col gap-4">
-      <MarkdownHooks>{problem}</MarkdownHooks>
-    </article>
+    <main className="p-8">
+      <article className="max-w-6xl mx-auto bg-sky-500 text-slate-200 p-8 my-8 rounded-lg text-lg flex flex-col gap-8 font-[Fira_Code]">
+        <MarkdownHooks components={components}>{problem}</MarkdownHooks>
+      </article>
+    </main>
   );
 }

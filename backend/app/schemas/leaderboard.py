b37662a3bcc76040f8505eb0739b0201edbdbb3f
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-02-17 16:40:04 -0800

    feat: add GET leaderboard endpoint

---
+++ backend/app/schemas/leaderboard.py
@@ -0,0 +1,8 @@
+from typing import List
+from pydantic import BaseModel
+
+class LeaderboardEntry(BaseModel):
+    team_id: int
+    team_name: str
+    total_points: float
+    solved_problems: List[str]

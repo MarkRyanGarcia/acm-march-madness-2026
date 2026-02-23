1fa8e86c4a09b8eb3e77f19b33329feb84a0e564
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-02-23 15:08:35 -0600

    remove tanstack devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,4 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackDevtools } from "@tanstack/react-devtools";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import type { GetToken, UserResource } from "@clerk/types";
 import { Navbar } from "@/components/Navbar";
@@ -22,22 +19,6 @@ export const Route = createRootRouteWithContext<AppRouterContext>()({
     <>
       <Navbar />
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-          openHotkey: ["Control", "A"],
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          {
-            name: "Tanstack Query",
-            render: <ReactQueryDevtoolsPanel />,
-          },
-        ]}
-      />
     </>
   ),
   notFoundComponent: NotFound,

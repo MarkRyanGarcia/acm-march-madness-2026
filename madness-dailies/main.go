79458024bd7c07afd00f2ce4b3c76bb7bc9e3747
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-02-27 15:14:09 -0600

    add requests for /problems/:day and /leaderboard endpoints

--- madness-dailies/main.go
+++ madness-dailies/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"cmp"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"slices"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -21,12 +24,75 @@ type WebhookPayload struct {
 	Embeds []Embed `json:"embeds"`
 }
 
+type ProblemInfo struct {
+	Title       string
+	Description string
+}
+
+func getProblem(apiURL string, day int) (string, error) {
+	resp, err := http.Get(apiURL + "/problems/" + strconv.Itoa(day))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	type ProblemResponse struct {
+		Problem string `json:"part1"`
+	}
+
+	problemResponse := ProblemResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(&problemResponse); err != nil {
+		return "", err
+	}
+
+	return problemResponse.Problem, nil
+}
+
+type TeamPointsEntry struct {
+	TeamName    string  `json:"team_name"`
+	TotalPoints float64 `json:"total_points"`
+}
+
+func getLeaderboard(apiURL string) ([]TeamPointsEntry, error) {
+	resp, err := http.Get(apiURL + "/leaderboard")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	leaderboard := []TeamPointsEntry{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&leaderboard); err != nil {
+		return nil, err
+	}
+
+	slices.SortFunc(leaderboard, func(a, b TeamPointsEntry) int {
+		if c := cmp.Compare(b.TotalPoints, a.TotalPoints); c != 0 {
+			return c
+		}
+		return cmp.Compare(a.TeamName, b.TeamName)
+	})
+
+	return leaderboard, nil
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Unabled to load .env: %v", err)
 	}
 	webhookURL := os.Getenv("WEBHOOK_URL")
+	apiURL := os.Getenv("API_URL")
+
+	problem, err := getProblem(apiURL, 0)
+	if err != nil {
+		log.Fatalf("Problem request failed: %v", err)
+	}
+	leaderboard, err := getLeaderboard(apiURL)
+	if err != nil {
+		log.Fatalf("Leaderboard request failed: %v", err)
+	}
+
+	log.Println(problem, leaderboard)
 
 	payload := WebhookPayload{
 		Embeds: []Embed{

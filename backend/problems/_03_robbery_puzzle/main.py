bdb1553579f8c6af4ff8a09138331c55d290f9b6
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-03-11 22:39:40 -0500

    add dot product solution, replace (r, c) with (x, y) instead

--- backend/problems/_03_robbery_puzzle/main.py
+++ backend/problems/_03_robbery_puzzle/main.py
@@ -4,9 +4,9 @@ from problems.base import Problem, main
 
 
 class RobberyPuzzle(Problem):
-    tubers: List[tuple[int, int]]  # (r, c)
-    guards: List[tuple[int, int, int]]  # (r, c, direction)
-    DIRS = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    tubers: List[tuple[int, int]]  # (x, y)
+    guards: List[tuple[int, int, int]]  # (x, y, direction)
+    DIRS = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
     SYMBOL_TO_DIR = {"^": 0, ">": 2, "v": 4, "<": 6}
 
     def __init__(self, seed=0) -> None:
@@ -24,40 +24,57 @@ class RobberyPuzzle(Problem):
                 continue
 
             if parsing_tubers:
-                r, c = map(int, line.split(","))
-                self.tubers.append((r, c))
+                x, y = map(int, line.split(","))
+                self.tubers.append((x, y))
             else:
                 coord, symbol = line.split(" ")
-                r, c = map(int, coord.split(","))
-                self.guards.append((r, c, self.SYMBOL_TO_DIR[symbol]))
+                x, y = map(int, coord.split(","))
+                self.guards.append((x, y, self.SYMBOL_TO_DIR[symbol]))
 
     def generate_input(self) -> str:
         return ""
 
-    def visible_atan(self, gr: int, gc: int, dir: int, tr: int, tc: int) -> bool:
-        dr = tr - gr
-        dc = tc - gc
-
-        if dr * dr + dc * dc > 100:
+    def visible_atan(self, gx: int, gy: int, dir: int, tx: int, ty: int) -> bool:
+        dx, dy = tx - gx, ty - gy
+        if dx * dx + dy * dy > 100:
             return False
 
-        tuber_angle = math.atan2(-dr, dc)
+        tuber_angle = math.atan2(dy, dx)
+        vx, vy = self.DIRS[dir]
+        if dx * vx + dy * vy <= 0:
+            return False
 
-        vr, vc = self.DIRS[dir]
-        guard_angle = math.atan2(-vr, vc)
+        guard_angle = math.atan2(vy, vx)
 
         diff = abs(tuber_angle - guard_angle)
         diff = min(diff, 2 * math.pi - diff)
 
         return diff <= math.pi / 4
 
+    def visible_dot(self, gx, gy, dir, tx, ty):
+        dx, dy = tx - gx, ty - gy
+        dist2 = dx * dx + dy * dy
+
+        if dist2 > 100:
+            return False
+
+        vx, vy = self.DIRS[dir]
+        dot = dx * vx + dy * vy
+
+        if dot <= 0:
+            return False
+
+        mag_v2 = vx * vx + vy * vy
+
+        return dot * dot * 2 >= mag_v2 * dist2
+
     def part1_sln(self) -> int:
         output = 0
 
-        for tr, tc in self.tubers:
+        for tx, ty in self.tubers:
             guarded = False
-            for gr, gc, dir in self.guards:
-                if self.visible_atan(gr, gc, dir, tr, tc):
+            for gx, gy, dir in self.guards:
+                if self.visible_atan(gx, gy, dir, tx, ty):
                     guarded = True
                     break
             if not guarded:
@@ -70,11 +87,11 @@ class RobberyPuzzle(Problem):
 
         for rot in range(8):
             stealable = 0
-            for tr, tc in self.tubers:
+            for tx, ty in self.tubers:
                 guarded = False
-                for gr, gc, dir in self.guards:
+                for gx, gy, dir in self.guards:
                     new_dir = (dir + rot) % 8
-                    if self.visible_atan(gr, gc, new_dir, tr, tc):
+                    if self.visible_atan(gx, gy, new_dir, tx, ty):
                         guarded = True
                         break
                 if not guarded:
